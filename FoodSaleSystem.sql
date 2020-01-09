27c4299a497b9b58eab884005d02724056abd7c8
xiaowang-bit <1632823097@qq.com>
2020-01-09 15:00:30 +0800

    厂库项目初始化

---
+++ FoodSaleSystem.sql
@@ -0,0 +1,130 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `id` INT(10) PRIMARY KEY,
+  `name` VARCHAR(100) NOT NULL,
+  `description` VARCHAR(255) DEFAULT NULL,
+  UNIQUE KEY `name` (`name`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of category
+-- ----------------------------
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` VARCHAR(100) NOT NULL,
+  `username` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(100)  NOT NULL,
+  `cellphone` VARCHAR(20) DEFAULT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS=1;
+-- ----------------------------
+-- Table structure for food
+-- ----------------------------
+DROP TABLE IF EXISTS `food`;
+CREATE TABLE `food` (
+  `id` VARCHAR(100),
+  `foodname` VARCHAR(100) NOT NULL,
+  `price` FLOAT(8,2) DEFAULT 0,
+  `imagepath` VARCHAR(300) DEFAULT NULL,
+  `description` VARCHAR(255) DEFAULT NULL,
+  `category_id` INT(10) DEFAULT 0,
+  PRIMARY KEY (`id`),
+  INDEX (`category_id`), 
+  CONSTRAINT `category_id_fk` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of food
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for orderinfo
+-- ----------------------------
+DROP TABLE IF EXISTS `orderinfo`;
+CREATE TABLE `orderinfo` (
+  `id` VARCHAR(100) NOT NULL,
+  `num` INT(11) DEFAULT NULL,
+  `price` FLOAT(8,2) DEFAULT NULL,
+  `status` INT(11) DEFAULT 0,
+  `user_id` VARCHAR(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id_fk` (`user_id`),
+  CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of orderinfo
+-- ----------------------------
+-- ----------------------------
+-- Table structure for orderitem
+-- ----------------------------
+DROP TABLE IF EXISTS `orderitem`;
+CREATE TABLE `orderitem` (
+  `id` VARCHAR(100) NOT NULL,
+  `num` INT(11) DEFAULT NULL,
+  `price` FLOAT(8,2) DEFAULT NULL,
+  `orderInfo_id` VARCHAR(100) DEFAULT NULL,
+  `food_id` VARCHAR(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `food_id_fk` (`food_id`),
+  KEY `orderInfo_id_fk` (`orderInfo_id`),
+  CONSTRAINT `food_id_fk` FOREIGN KEY (`food_id`) REFERENCES `food` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `orderInfo_id_fk` FOREIGN KEY (`orderInfo_id`) REFERENCES `orderinfo` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of orderitem
+-- ----------------------------
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` VARCHAR(100) NOT NULL,
+  `username` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(100) DEFAULT NULL,
+  `cellphone` VARCHAR(20) DEFAULT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+SET FOREIGN_KEY_CHECKS=1;
+-- ----------------------------
+-- Table structure for address
+-- ----------------------------
+DROP TABLE IF EXISTS `address`;
+CREATE TABLE `address` (
+  `id` VARCHAR(100) NOT NULL,
+  `address` VARCHAR(100) NOT NULL,
+  `user_id` VARCHAR(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `u_id_fk` (`user_id`),
+  CONSTRAINT `u_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of address
+-- ----------------------------
+SET FOREIGN_KEY_CHECKS=1;
---
+++ FoodSaleSystem/src/com/hubu/utils/UuidUtil.java
@@ -0,0 +1,23 @@
+package com.hubu.utils;
+
+import java.util.UUID;
+
+/**
+ * 产生UUID随机字符串工具类
+ */
+public final class UuidUtil {
+	private UuidUtil(){}
+	public static String getUuid(){
+		return UUID.randomUUID().toString().replace("-","");
+	}
+	/**
+	 * 测试
+	 */
+	public static void main(String[] args) {
+		//全球唯一，电脑硬件，当时时间，算法，随机出现的id
+		System.out.println(UuidUtil.getUuid());
+		System.out.println(UuidUtil.getUuid());
+		System.out.println(UuidUtil.getUuid());
+		System.out.println(UuidUtil.getUuid());
+	}
+}
